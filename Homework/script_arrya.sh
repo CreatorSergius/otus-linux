88c41615fb5067ac31c17fae1e28e236174ec2f6
desktop kiryakov <s.kiryakov@ixv.ru>
2020-02-21 17:42:39 +0300

    HW_1

--- Homework/script_arrya.sh
+++ Homework/script_arrya.sh
@@ -1,5 +1,5 @@
+#!/bin/bahs
 #Список команд создания массива и файловой системы.
-
 sudo mdadm --zero-superblock --verbose /dev/sd{b,c,d,e,f}  
 sudo mdadm  --create /dev/md0 --level=5 --raid-device 5 /dev/sd{b,c,d,e,f}      
 sudo mkdir -p /etc/mdadm
@@ -15,7 +15,7 @@ parted /dev/md0 mkpart primari ext4 80% 100%
 for i in $(seq 1 5); do mkfs.ext4 /dev/md0p$i; done
 mkdir -p /raid/part{1,2,3,4,5}
 for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
-
+for i in $(seq 1 5); do echo /dev/md0p$i /raid/part$i ext4 defaults 0 0 >> /etc/fstab; done
 #mdadm /dev/md0 --fail /dev/sdf
 #mdadm /dev/md0 --remove /dev/sdf
 #mdadm /dev/md0 --add /dev/sdg
